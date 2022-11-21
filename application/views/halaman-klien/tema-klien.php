22c3af6442516a3b89cb49689c6e334d40419a87
dimyati000 <dimyatiazhar000@gmail.com>
2022-11-21 11:13:25 +0700

    perbaikan halaman klien

--- application/views/halaman-klien/tema-klien.php
+++ application/views/halaman-klien/tema-klien.php
@@ -12,34 +12,34 @@
 		<link rel="apple-touch-icon" href="apple-touch-icon.png">
 		<link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url() ?>assets/images/fav.png">
 		<!-- Bootstrap v4.4.1 css -->
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/klien/css/bootstrap.min.css">
 		<!-- font-awesome css -->
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/klien/css/font-awesome.min.css">
 		<!-- Uicons Regular Rounded css -->
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/uicons-regular-rounded.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/klien/css/uicons-regular-rounded.css">
 		<!-- flaticon css -->
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/fonts/flaticon.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/klien/fonts/flaticon.css">
 		<!-- animate css -->
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/animate.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/klien/css/animate.css">
 		<!-- slick css -->
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/klien/css/slick.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/klien/klien/css/slick.css">
 		<!-- owl.carousel css -->
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/klien/css/owl.carousel.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/klien/css/owl.carousel.css">
 		<!-- off canvas css -->
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/klien/css/off-canvas.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/klien/css/off-canvas.css">
 		<!-- magnific popup css -->
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/klien/css/magnific-popup.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/klien/css/magnific-popup.css">
 		<!-- Main Menu css -->
 		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/klien/css/rsmenu-main.css">
 		<!-- nivo slider CSS -->
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/klien/inc/custom-slider/css/nivo-slider.css">
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/klien/inc/custom-slider/css/preview.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/klien/inc/custom-slider/css/nivo-slider.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/klien/inc/custom-slider/css/preview.css">
 		<!-- spacing css -->
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/klien/css/rs-spacing.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/klien/css/rs-spacing.css">
 		<!-- style css -->
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/klien/css/style.css"> <!-- This stylesheet dynamically changed from style.less -->
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/klien/css/style.css"> <!-- This stylesheet dynamically changed from style.less -->
 		<!-- responsive css -->
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/klien/css/responsive.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/klien/css/responsive.css">
 		
 	</head>
 	<body class="defult-home">
