cec6473f2ac7ccf6b507ace1d03bca0976187baf
dimyati000 <dimyatiazhar000@gmail.com>
2022-11-28 06:19:31 +0700

    halaman pelayanan dan halaman form konsultasi sekarang

--- application/controllers/Jasa.php
+++ application/controllers/Jasa.php
@@ -3,8 +3,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Jasa extends CI_Controller{
 
     public function perpajakan(){
-        $data['title'] = "Perpajakan | Kantor Konsultan Pajak dan Hukum";
-        $data['konten'] = "perpajakan.php";    
+        $data['title'] = "Jasa Perpajakan | Kantor Konsultan Pajak dan Hukum";
+        $data['konten'] = "layanan-klien/jasa-perpajakan.php";
+
+        $this->load->view('halaman-klien/tema-klien.php', $data);
+    }
+
+    public function akuntansi(){
+        $data['title'] = "Jasa Akuntanasi | Kantor Konsultan Pajak dan Hukum";
+        $data['konten'] = "layanan-klien/jasa-akuntanasi.php";    
+
+        $this->load->view('halaman-klien/tema-klien.php', $data);
+    }
+
+    public function hukum(){
+        $data['title'] = "Jasa Hukum | Kantor Konsultan Pajak dan Hukum";
+        $data['konten'] = "layanan-klien/jasa-hukum.php";
+
+        $this->load->view('halaman-klien/tema-klien.php', $data);
+    }
+
+    public function form_konsultasi(){
+        $data['title'] = "Form Konsultasi | Kantor Konsultan Pajak dan Hukum";
+        $data['konten'] = "layanan-klien/form-konsultasi.php";
 
         $this->load->view('halaman-klien/tema-klien.php', $data);
     }
---
+++ application/views/halaman-klien/layanan-klien/jasa-perpajakan.php
@@ -0,0 +1,388 @@
+        
+			<!-- Breadcrumbs Start -->
+			<div class="rs-breadcrumbs img2">
+			    <div class="container">
+			        <div class="breadcrumbs-inner">
+			            <h1 class="page-title">
+			               	Jasa Perpajakan
+			            </h1>
+			            <span class="sub-text">Kami melakukan yang terbaik dalam melayani anda pada segala kasus.</span>
+			            <ul class="breadcrumbs-area">   
+                           <li title="Go to konstruk">
+                               <a class="active" href="<?= site_url('') ?>">Home</a>
+                           </li>
+                          <li>Jasa Perpajakan</li>
+                       </ul>
+			        </div>
+			    </div>
+			</div>
+			<!-- Breadcrumbs End -->
+
+			<!-- Pricing Section Start -->
+			<div class="rs-pricing pricing-style1 pt-120 pb-120 md-pt-80 md-pb-80">
+	        	<div class="container">
+	        		<div class="row">
+	        			<div class="col-lg-4 col-md-6 md-mb-50">
+	        				<div class="pricing-table-wrap">
+	        					<div class="pricing-table-header">
+	        					    <h3 class="pricing-table-title">Konsultasi Pelaporan Pajak Bulanan</h3>
+	        					</div>
+	        					<div class="pricing-icon">
+	        						<img src="<?php echo base_url() ?>assets/klien/images/services/main-home/main-icons/AK.png" alt="Images">
+	        					</div>
+	        					<div class="pricing-table-price-tag">
+				                    <span class="pricing-table-currency">$</span>
+				                    <span class="pricing-table-price-text">3500</span>
+				                    <span class="pricing-table-period">Month</span>
+				                </div>
+				                <ul class="pricing-table-features-list">
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Target estimate</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Cost plus fixed fee</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-close"></i>
+				                		<span class="feature-text">Professional Design</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-close"></i>
+				                		<span class="feature-text">10 Year maintenance</span>
+				                	</li>
+				                </ul>
+				                <div class="btn-part mt-45 md-mt-30 text-center">
+				                    <a class="readon more pricing" href="<?= site_url('Jasa/Form_Konsultasi') ?>"><span>Konsultasi Sekarang</span></a>
+				                </div>
+	        				</div>
+	        			</div>
+	        			<div class="col-lg-4 col-md-6 md-mb-50">
+	        				<div class="pricing-table-wrap">
+	        					<div class="pricing-table-header">
+	        					    <h3 class="pricing-table-title">Konsultasi Pelaporan Pajak Tahunan</h3>
+	        					</div>
+	        					<div class="pricing-icon">
+	        						<img src="<?php echo base_url() ?>assets/klien/images/services/main-home/main-icons/AK.png" alt="Images">
+	        					</div>
+	        					<div class="pricing-table-price-tag">
+				                    <span class="pricing-table-currency">$</span>
+				                    <span class="pricing-table-price-text">4500</span>
+				                    <span class="pricing-table-period">Month</span>
+				                </div>
+				                <ul class="pricing-table-features-list">
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Target estimate</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Cost plus fixed fee</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Professional Design</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-close"></i>
+				                		<span class="feature-text">10 Year maintenance</span>
+				                	</li>
+				                </ul>
+				                <div class="btn-part mt-45 md-mt-30 text-center">
+				                    <a class="readon more pricing" href="<?= site_url('Jasa/Form_Konsultasi') ?>"><span>Konsultasi Sekarang</span></a>
+				                </div>
+	        				</div>
+	        			</div>
+	        			<div class="col-lg-4 col-md-6 md-mb-50">
+	        				<div class="pricing-table-wrap">
+	        					<div class="pricing-table-header">
+	        					    <h3 class="pricing-table-title">Konsultasi Permasalahan Pajak</h3>
+	        					</div>
+	        					<div class="pricing-icon">
+	        						<img src="<?php echo base_url() ?>assets/klien/images/services/main-home/main-icons/AK.png" alt="Images">
+	        					</div>
+	        					<div class="pricing-table-price-tag">
+				                    <span class="pricing-table-currency">$</span>
+				                    <span class="pricing-table-price-text">6500</span>
+				                    <span class="pricing-table-period">Month</span>
+				                </div>
+				                <ul class="pricing-table-features-list">
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Target estimate</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Cost plus fixed fee</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Professional Design</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-close"></i>
+				                		<span class="feature-text">10 Year maintenance</span>
+				                	</li>
+				                </ul>
+				                <div class="btn-part mt-45 md-mt-30 text-center">
+				                    <a class="readon more pricing" href="<?= site_url('Jasa/Form_Konsultasi') ?>"><span>Konsultasi Sekarang</span></a>
+				                </div>
+	        				</div>
+	        			</div>
+	        			<div class="col-lg-4 col-md-6 md-mb-50 pt-50">
+	        				<div class="pricing-table-wrap">
+	        					<div class="pricing-table-header">
+	        					    <h3 class="pricing-table-title">Konsultasi Perencanaan Pajak</h3>
+	        					</div>
+	        					<div class="pricing-icon">
+	        						<img src="<?php echo base_url() ?>assets/klien/images/services/main-home/main-icons/AK.png" alt="Images">
+	        					</div>
+	        					<div class="pricing-table-price-tag">
+				                    <span class="pricing-table-currency">$</span>
+				                    <span class="pricing-table-price-text">6500</span>
+				                    <span class="pricing-table-period">Month</span>
+				                </div>
+				                <ul class="pricing-table-features-list">
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Target estimate</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Cost plus fixed fee</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Professional Design</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-close"></i>
+				                		<span class="feature-text">10 Year maintenance</span>
+				                	</li>
+				                </ul>
+				                <div class="btn-part mt-45 md-mt-30 text-center">
+				                    <a class="readon more pricing" href="<?= site_url('Jasa/Form_Konsultasi') ?>"><span>Konsultasi Sekarang</span></a>
+				                </div>
+	        				</div>
+	        			</div>
+	        			<div class="col-lg-4 col-md-6 md-mb-50 pt-50">
+	        				<div class="pricing-table-wrap">
+	        					<div class="pricing-table-header">
+	        					    <h3 class="pricing-table-title">Laporan Keuangan Fiskal</h3>
+	        					</div>
+	        					<div class="pricing-icon">
+	        						<img src="<?php echo base_url() ?>assets/klien/images/services/main-home/main-icons/AK.png" alt="Images">
+	        					</div>
+	        					<div class="pricing-table-price-tag">
+				                    <span class="pricing-table-currency">$</span>
+				                    <span class="pricing-table-price-text">6500</span>
+				                    <span class="pricing-table-period">Month</span>
+				                </div>
+				                <ul class="pricing-table-features-list">
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Target estimate</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Cost plus fixed fee</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Professional Design</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-close"></i>
+				                		<span class="feature-text">10 Year maintenance</span>
+				                	</li>
+				                </ul>
+				                <div class="btn-part mt-45 md-mt-30 text-center">
+				                    <a class="readon more pricing" href="<?= site_url('Jasa/Form_Konsultasi') ?>"><span>Konsultasi Sekarang</span></a>
+				                </div>
+	        				</div>
+	        			</div>
+	        			<div class="col-lg-4 col-md-6 md-mb-50 pt-50">
+	        				<div class="pricing-table-wrap">
+	        					<div class="pricing-table-header">
+	        					    <h3 class="pricing-table-title">Konsultasi Laporan Komersial</h3>
+	        					</div>
+	        					<div class="pricing-icon">
+	        						<img src="<?php echo base_url() ?>assets/klien/images/services/main-home/main-icons/AK.png" alt="Images">
+	        					</div>
+	        					<div class="pricing-table-price-tag">
+				                    <span class="pricing-table-currency">$</span>
+				                    <span class="pricing-table-price-text">6500</span>
+				                    <span class="pricing-table-period">Month</span>
+				                </div>
+				                <ul class="pricing-table-features-list">
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Target estimate</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Cost plus fixed fee</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Professional Design</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-close"></i>
+				                		<span class="feature-text">10 Year maintenance</span>
+				                	</li>
+				                </ul>
+				                <div class="btn-part mt-45 md-mt-30 text-center">
+				                    <a class="readon more pricing" href="<?= site_url('Jasa/Form_Konsultasi') ?>"><span>Konsultasi Sekarang</span></a>
+				                </div>
+	        				</div>
+	        			</div>
+	        			<div class="col-lg-4 col-md-6 md-mb-50 pt-50">
+	        				<div class="pricing-table-wrap">
+	        					<div class="pricing-table-header">
+	        					    <h3 class="pricing-table-title">Pendampingan SP2DK</h3>
+	        					</div>
+	        					<div class="pricing-icon">
+	        						<img src="<?php echo base_url() ?>assets/klien/images/services/main-home/main-icons/AK.png" alt="Images">
+	        					</div>
+	        					<div class="pricing-table-price-tag">
+				                    <span class="pricing-table-currency">$</span>
+				                    <span class="pricing-table-price-text">6500</span>
+				                    <span class="pricing-table-period">Month</span>
+				                </div>
+				                <ul class="pricing-table-features-list">
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Target estimate</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Cost plus fixed fee</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Professional Design</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-close"></i>
+				                		<span class="feature-text">10 Year maintenance</span>
+				                	</li>
+				                </ul>
+				                <div class="btn-part mt-45 md-mt-30 text-center">
+				                    <a class="readon more pricing" href="<?= site_url('Jasa/Form_Konsultasi') ?>"><span>Konsultasi Sekarang</span></a>
+				                </div>
+	        				</div>
+	        			</div>
+	        			<div class="col-lg-4 col-md-6 md-mb-50 pt-50">
+	        				<div class="pricing-table-wrap">
+	        					<div class="pricing-table-header">
+	        					    <h3 class="pricing-table-title">Pendampingan Pemeriksaan Pajak</h3>
+	        					</div>
+	        					<div class="pricing-icon">
+	        						<img src="<?php echo base_url() ?>assets/klien/images/services/main-home/main-icons/AK.png" alt="Images">
+	        					</div>
+	        					<div class="pricing-table-price-tag">
+				                    <span class="pricing-table-currency">$</span>
+				                    <span class="pricing-table-price-text">6500</span>
+				                    <span class="pricing-table-period">Month</span>
+				                </div>
+				                <ul class="pricing-table-features-list">
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Target estimate</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Cost plus fixed fee</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Professional Design</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-close"></i>
+				                		<span class="feature-text">10 Year maintenance</span>
+				                	</li>
+				                </ul>
+				                <div class="btn-part mt-45 md-mt-30 text-center">
+				                    <a class="readon more pricing" href="<?= site_url('Jasa/Form_Konsultasi') ?>"><span>Konsultasi Sekarang</span></a>
+				                </div>
+	        				</div>
+	        			</div>
+	        			<div class="col-lg-4 col-md-6 md-mb-50 pt-50">
+	        				<div class="pricing-table-wrap">
+	        					<div class="pricing-table-header">
+	        					    <h3 class="pricing-table-title">Pendampingan Keberatan Pajak</h3>
+	        					</div>
+	        					<div class="pricing-icon">
+	        						<img src="<?php echo base_url() ?>assets/klien/images/services/main-home/main-icons/AK.png" alt="Images">
+	        					</div>
+	        					<div class="pricing-table-price-tag">
+				                    <span class="pricing-table-currency">$</span>
+				                    <span class="pricing-table-price-text">6500</span>
+				                    <span class="pricing-table-period">Month</span>
+				                </div>
+				                <ul class="pricing-table-features-list">
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Target estimate</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Cost plus fixed fee</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Professional Design</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-close"></i>
+				                		<span class="feature-text">10 Year maintenance</span>
+				                	</li>
+				                </ul>
+				                <div class="btn-part mt-45 md-mt-30 text-center">
+				                    <a class="readon more pricing" href="<?= site_url('Jasa/Form_Konsultasi') ?>"><span>Konsultasi Sekarang</span></a>
+				                </div>
+	        				</div>
+	        			</div>
+	        			<div class="col-lg-4 col-md-6 md-mb-50 pt-50">
+	        				<div class="pricing-table-wrap">
+	        					<div class="pricing-table-header">
+	        					    <h3 class="pricing-table-title"> Keberatan Pajak</h3>
+	        					</div>
+	        					<div class="pricing-icon">
+	        						<img src="<?php echo base_url() ?>assets/klien/images/services/main-home/main-icons/AK.png" alt="Images">
+	        					</div>
+	        					<div class="pricing-table-price-tag">
+				                    <span class="pricing-table-currency">$</span>
+				                    <span class="pricing-table-price-text">6500</span>
+				                    <span class="pricing-table-period">Month</span>
+				                </div>
+				                <ul class="pricing-table-features-list">
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Target estimate</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Cost plus fixed fee</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-check"></i>
+				                		<span class="feature-text">Professional Design</span>
+				                	</li>
+				                	<li>
+				                		<i class="fa fa-close"></i>
+				                		<span class="feature-text">10 Year maintenance</span>
+				                	</li>
+				                </ul>
+				                <div class="btn-part mt-45 md-mt-30 text-center">
+				                    <a class="readon more pricing" href="<?= site_url('Jasa/Form_Konsultasi') ?>"><span>Konsultasi Sekarang</span></a>
+				                </div>
+	        				</div>
+	        			</div>
+	        		</div>
+	        	</div>
+			</div>
+			<!-- Pricing Section End -->
