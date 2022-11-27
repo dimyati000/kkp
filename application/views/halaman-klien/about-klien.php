22f3fbef2416d3252b6e9309e53d082b0ebfcaf8
Nihlatulia98 <rahmahtiyas@gmail.com>
2022-11-27 11:44:39 +0700

    tampilan about

---
+++ application/views/halaman-klien/about-klien.php
@@ -0,0 +1,260 @@
+
+			<!-- Breadcrumbs Start -->
+			<div class="rs-breadcrumbs img1">
+			    <div class="container">
+			        <div class="breadcrumbs-inner">
+			            <h1 class="page-title">
+			               	About Us
+			            </h1>
+			            <span class="sub-text">Excepteur sint occaecat cupidatat non proident, sunt in coulpa qui official<br> 
+			            modeserunt mollit anim id est 20 years experience.</span>
+			            <ul class="breadcrumbs-area">
+                           <li title="Go to konstruk">
+                               <a class="active" href="index.html">Home</a>
+                           </li>
+                          <li>About</li>
+                       </ul>
+			        </div>
+			    </div>
+			</div>
+			<!-- Breadcrumbs End -->
+		
+			<!-- About Start -->
+			<div class="rs-about about-style1 bg1 pt-120 pb-120 md-pt-80 md-pb-75">
+				<div class="container">
+					<div class="row y-middle">
+						<div class="col-lg-6 md-mb-50">
+							<div class="images-part">
+								<img src="assets/images/about/style1/about.png" alt="About">
+							</div>
+						</div>
+						<div class="col-lg-6 pl-50 md-pl-15">
+							<div class="sec-title">
+								<span class="sub-text">
+									<img src="assets/images/shape-1.png" alt="Images">
+									Who We Are
+								</span>
+								<h2 class="title pb-20">
+									Construction solutions focused future ready
+								</h2>
+								<p class="desc pb-37">
+									Leverage agile frameworks to provide a robust synopsis for high 
+									level overviews. Iterative approaches to corporate strategy data 
+									foster to collaborative thinking to empowerment.
+								</p>
+								<div class="row y-middle">
+									<div class="col-lg-4 col-md-4 sm-mb-30">
+										<div class="about-img">
+											<img src="assets/images/about/style1/abzt.jpg" alt="About">
+										</div>
+									</div>
+									<div class="col-lg-8 col-md-8">
+										<ul class="check-lists">
+				        					<li class="list-item">
+												<span class="icon-list-icon">
+													<i class="fa fa-check-circle"></i>						
+												</span>
+												<span class="list-text">Pre-construction services</span>
+											</li>
+											<li class="list-item">
+												<span class="icon-list-icon">
+													<i class="fa fa-check-circle"></i>						
+												</span>
+												<span class="list-text">Industrial manufacturing</span>
+											</li>
+											<li class="list-item">
+												<span class="icon-list-icon">
+													<i class="fa fa-check-circle"></i>						
+												</span>
+												<span class="list-text">Building and construction</span>
+											</li>
+				        				</ul>
+									</div>
+								</div>
+								<div class="row mt-47 md-mt-25">
+									<div  class="col-lg-6 col-md-4 sm-mb-30">
+										<div class="btn-part">
+											<a class="readon more know" href="about.html">Know more</a>
+										</div>
+									</div>
+									<div class="col-lg-6 col-md-8">
+										<div class="signeture-img">
+											<img src="assets/images/about/style1/signeture1.png" alt="Images">
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- About End -->
+
+			<!-- Skillbar Choose Start -->
+			<div class="rs-skillbar skillbar-style1 skillbar-modify5 bg22 pt-120 pb-120">
+                <div class="container">
+	                <div class="row y-middle">
+	                	<div class="col-lg-6 pr-60 md-pr-15 mt-40 md-mb-40">
+		                	<div class="sec-title mb-45">
+		                		<h2 class="title title4 pb-30">
+		                			Our management skills and work
+		                			experience
+		                		</h2>
+		                		<p class="desc desc8">
+									Podcasting operational change management inside of workflows to<br> 
+									establish a framework. Taking seamless key performance.
+								</p>
+		                	</div>
+		        	        <!-- Skillbar Section Start -->
+		                    <div class="cl-skill-bar">
+		                        <!-- Start Skill Bar -->
+		                        <span class="skillbar-title">Planning</span>
+		                        <div class="skillbar" data-percent="85">
+		                            <p class="skillbar-bar"></p>
+		                            <span class="skill-bar-percent">85%</span> 
+		                        </div> 
+		                        <!-- Start Skill Bar -->
+		                        <span class="skillbar-title">Design</span>
+		                        <div class="skillbar" data-percent="95">
+		                            <p class="skillbar-bar"></p>
+		                            <span class="skill-bar-percent">95%</span> 
+		                        </div> 
+		                        <!-- Start Skill Bar -->
+		                        <span class="skillbar-title">Development</span>
+		                        <div class="skillbar" data-percent="75">
+		                            <p class="skillbar-bar"></p>
+		                            <span class="skill-bar-percent">75%</span> 
+		                        </div> 
+		                        <!-- Start Skill Bar -->
+		                        <span class="skillbar-title">Delivery</span>
+		                        <div class="skillbar" data-percent="90">
+		                            <p class="skillbar-bar"></p>
+		                            <span class="skill-bar-percent">90%</span> 
+		                        </div>
+		                    </div>
+		        	        <!-- Skillbar Section End -->
+	                	</div>
+						<div class="col-lg-6 video-img">
+							<div class="rs-videos">
+								<div class="animate-border">
+	                                <a class="popup-border popup-videos" href="https://www.youtube.com/watch?v=FMvA5fyZ338">
+	                                    <i class="fa fa-play"></i>
+	                                </a>
+	                            </div>    
+							</div>
+						</div>
+	                </div>
+                </div>
+            </div>
+			<!-- Skillbar Choose End -->
+
+			<!-- Contact Section Start -->
+			<div class="rs-contact contact-style1 bg3">
+	   	    	<div class="row">
+	   	    		<div class="col-lg-4 offset-2 con-left-6">
+	   					<div class="contact-section">
+	   	                    <div class="contact-wrap">
+	   	                    	<div class="sec-title mb-45">
+	   	                    		<span class="sub-text">
+	   	                    			<img src="assets/images/shape-1.png" alt="Images">
+	   	                    			Lets talk
+	   	                    		</span>
+	   	                    		<h2 class="title">
+	   	                    			Have an upcoming project?
+	   	                    		</h2>
+	   	                    	</div>
+	   	                        <div id="form-messages"></div>
+	   	                        <form id="contact-form" method="post" action="mailer.php">
+	   	                            <fieldset>
+	   	                                <div class="row">
+	   	                                    <div class="col-lg-6 col-md-6 col-sm-6 mb-30">
+	   	                                        <input class="from-control" type="text" id="name" name="name" placeholder="Name" required="">
+	   	                                    </div> 
+	   	                                    <div class="col-lg-6 col-md-6 col-sm-6 mb-30">
+	   	                                        <input class="from-control" type="text" id="email" name="email" placeholder="E-Mail" required="">
+	   	                                    </div>   
+	   	                                    <div class="col-lg-6 col-md-6 col-sm-6 mb-30">
+	   	                                        <input class="from-control" type="text" id="phone" name="phone" placeholder="Phone Number" required="">
+	   	                                    </div>
+	   	                                    <div class="col-lg-6 col-md-6 col-sm-6 mb-30">
+	   	                                        <input class="from-control" type="text" id="website" name="website" placeholder="Your Website" required="">
+	   	                                    </div>
+	   	                                    <div class="col-lg-12 mb-30">
+	   	                                        <textarea class="from-control" id="message" name="message" placeholder="Your Message Here" required=""></textarea>
+	   	                                    </div>
+	   	                                </div>
+	   	                                <div class="btn-part">                                            
+	   	                                    <div class="form-group mb-0">
+	   	                                        <input class="readon more submit" type="submit" value="Submit Now">
+	   	                                    </div>
+	   	                                </div> 
+	   	                            </fieldset>
+	   	                        </form> 
+	   	                    </div>
+	   	                </div>
+	   	    		</div>
+	   	    		<div class="col-lg-2 pr-50 md-pr-15 con-right-6 md-mt-50">
+	   	    			<div class="contact-box-item">
+				    		<div class="contact-box mb-20">
+			                    <div class="contact-icon">
+			                        <img src="assets/images/contact/style1/icons/1.png" alt="images">
+			                    </div>
+			                    <div class="content-text">
+			                        <h4 class="title"><a href="#">USA office</a></h4>
+			                        <p class="services-txt">55 Gerad Lane,<br> NY 11201, USA</p>
+			                    </div>
+			                </div>
+			                <div class="contact-box mb-20">
+			                    <div class="contact-icon">
+			                        <img src="assets/images/contact/style1/icons/2.png" alt="images">
+			                    </div>
+			                    <div class="content-text">
+			                        <h4 class="title"><a href="#">Email us</a></h4>
+			                        <p class="services-txt">
+			                        	<a href="mailto:test@gamil.com">test@gamil.com</a><br>
+			                        	<a href="mailto:test2@gamil.com">test2@gamil.com</a>
+			                        </p>
+			                    </div>
+			                </div>
+			                <div class="contact-box">
+			                    <div class="contact-icon">
+			                        <img src="assets/images/contact/style1/icons/3.png" alt="images">
+			                    </div>
+			                    <div class="content-text">
+			                        <h4 class="title"><a href="#">Call us</a></h4>
+			                        <p class="services-txt">
+			                        	<a href="tel:(+088)589-8745">(+088) 589-8745</a><br>
+			                        	<a href="tel:(+088)222-9999">(+088) 222-9999</a>
+			                        </p>
+			                    </div>
+			                </div>
+	   	    			</div>
+	   	    		</div>
+	   	    		<div class="col-lg-4 contact-right-img"></div>
+	   	    	</div>
+			</div>
+			<!-- Contact Section End -->
+
+			<!-- Call To Action Choose Start -->
+			<div class="rs-cta bg4 pt-120 pb-115 md-pt-80 md-pb-75">
+				<div class="container">
+					<div class="call-action">
+						<div class="sec-title text-center">
+							<span class="sub-text yellow-color">
+								Work With Us !
+							</span>
+							<h2 class="title white-color pb-20">
+								Have any upcoming project
+							</h2>
+							<p class="desc desc3 pb-35">
+								Leverage agile frameworks to provide a robust synopsis for high level overviews.<br> 
+								Iterative approaches to corporate strategy foster collaborative thinking
+							</p>
+							<div class="btn-part">
+								<a class="readon more contact" href="contact.html">Get Started</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Call To Action Choose End -->
