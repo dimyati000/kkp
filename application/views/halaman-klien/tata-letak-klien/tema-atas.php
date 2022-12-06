91f764140b4299e0a996651c16058d44582f9b17
dimyati000 <dimyatiazhar000@gmail.com>
2022-12-06 12:56:21 +0700

    penyesuaian jam pelayanan

--- application/views/halaman-klien/tata-letak-klien/tema-atas.php
+++ application/views/halaman-klien/tata-letak-klien/tema-atas.php
@@ -3,7 +3,8 @@
 		<div class="toolbar-area topbar-style1 hidden-md">
 			<div class="container">
 				<div class="row rs-vertical-middle">
-					<div class="col-lg-7">
+					<div class="col-lg-10">
+					<!-- <div class="col-lg-12 col-md-10 col-sm-11"> -->
 						<div class="toolbar-contact">
 							<ul class="rs-contact-info">
 								<li>
@@ -16,22 +17,29 @@
 								</li>
 								<li>
 									<i class="fi-rr-map-marker-home"></i>
-									Sidoarjo
+									<a href="https://goo.gl/maps/mM29wiJFTbewjQ1A9">Sidoarjo</a>
+								</li>
+								<li>
+									<i class="fi-rr-time-add"></i>
+									Senin - Sabtu: 8.00 am - 5.00 pm
 								</li>
 							</ul>
 						</div>
 					</div>
-					<div class="col-lg-5">
+					<div class="col-lg-2">
+					<!-- <div class="col-lg-12 col-md-2 col-sm-1"> -->
 						<div class="toolbar-sl-share">
 							<ul class="clearfix">
-								<li class="opening">
+								<li class="opening" style="align:center; justify:center">
 									<a href="<?php echo site_url('Klien/translate/in'); ?>">
-										<img style="width:5%" src="<?php echo base_url() ?>assets/img/bahasa-id.png">
+										<img style="width:12%" src="<?php echo base_url() ?>assets/img/bahasa-id.png">
 										<em> ID</em>
+										<!-- <em class="ml-5">ID</em> -->
 									</a>
-								 	<a class="ml-15" href="<?php echo site_url('Klien/translate/en'); ?>">
-										<img style="width:5%" src="<?php echo base_url() ?>assets/img/bahasa-uk.png">
+								 	<a class="ml-12" href="<?php echo site_url('Klien/translate/en'); ?>">
+										<img style="width:12%" src="<?php echo base_url() ?>assets/img/bahasa-uk.png">
 										<em> EN</em>
+										<!-- <em class="ml-5">EN</em> -->
 									</a>
 								</li>
 							</ul>
